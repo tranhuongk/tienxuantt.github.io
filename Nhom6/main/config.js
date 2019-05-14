6719c618ad696fc93fce8bde182d809c9e010fcc
= Xuan <=tienxuantt@gmail.com>
2019-05-14 18:51:03 +0700

    update

--- Nhom6/main/config.js
+++ Nhom6/main/config.js
@@ -10,7 +10,7 @@ var IconPage = L.Icon.extend({
     }
 });
 IconCurrent = new IconPage({ iconUrl: "../Nhom6/images/police.png"});
-for(var i = 0; i < array1.length; i+=2){
-    notifi = array2[i/2] + "<br/> Địa chỉ: "+ array3[i/2];
-    L.marker([array1[i+1], array1[i]], {icon: IconCurrent}).bindPopup(notifi).addTo(map);
+for(var i = 0; i < policearray1.length; i+=2){
+    notifi = policearray2[i/2] + "<br/> Địa chỉ: "+ policearray3[i/2];
+    L.marker([policearray1[i+1], policearray1[i]], {icon: IconCurrent}).bindPopup(notifi).addTo(map);
 }
